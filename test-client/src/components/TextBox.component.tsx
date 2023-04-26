600c21e5f90ece299bf4108a41a3ff4707b2c914
riz-j <adiputra979@gmail.com>
2023-04-26 15:54:35 +1000

    Blank slate

--- test-client/src/components/TextBox.component.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function TextBox() {
-    return (
-        <div>Is it me you're looking for?</div>
-    )
-}
--- test-client/src/containers/Chatbox.container.tsx
+++ test-client/src/containers/Chatbox.container.tsx
@@ -1,10 +1,8 @@
-import TextBox from "components/TextBox.component.tsx"
 
 export default function ChatBox() {
     return (
         <>
             <div>Hello World!</div>
-            <TextBox />
         </>
     )
 }

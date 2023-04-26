17ae9a3c18fadde6697da045d0fd0ba2082d09aa
riz-j <adiputra979@gmail.com>
2023-04-26 17:43:08 +1000

    Add disconnect event

--- websocket/behavior/base.py
+++ websocket/behavior/base.py
@@ -5,5 +5,9 @@ async def connect(sid, environ):
     print(f"{sid} joined the server")
 
 @sio.event
+def disconnect(sid):
+    print(f"{sid} disconnected")
+
+@sio.event
 async def onMessage(sid, message):
     print(f"Message from client: {message}")

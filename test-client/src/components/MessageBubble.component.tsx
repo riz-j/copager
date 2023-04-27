dd18deabf407c07d43fdaf10e9e171cb9df1b432
riz-j <adiputra979@gmail.com>
2023-04-27 14:22:03 +1000

    Test: make sure the Message object is properly transferred

--- test-client/src/components/MessageBubble.component.tsx
+++ test-client/src/components/MessageBubble.component.tsx
@@ -8,7 +8,13 @@ const MessageBubble: React.FC<MessageBubbleProps> = (props) => {
     return (
         <>
             <div className="bg-blue-100 my-1 mx-10 py-2 px-3 rounded-xl w-1/2">
-                {props.message.message}
+                <p>{props.message.message}</p>
+                {/* <p>ID:   {props.message.id_}</p> */}
+                {/* <p>Type:   {props.message.type}</p> */}
+                {/* <p>Message:   {props.message.message}</p>  */}
+                {/* <p>Timestamp:   {props.message.timestamp}</p>  */}
+                {/* <p>Sender:   {props.message.sender}</p>  */}
+                {/* <p>Room:   {props.message.room}</p>  */}
             </div>
         </>
     )

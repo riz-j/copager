3c350ff5ed48fe167f0ad6254925a9f1b6c382c5
riz-j <adiputra979@gmail.com>
2023-04-27 10:24:47 +1000

    Add handleKeyDown

--- test-client/src/containers/Chatbox.container.tsx
+++ test-client/src/containers/Chatbox.container.tsx
@@ -1,32 +1,43 @@
 import { useState } from "react";
 import { useSocket } from "hooks/useSocket";
 
-export default function ChatBox() {
+const ChatBox: React.FC = () => {
   const socket = useSocket();
   /* 
         IMPROVEMENT: declaring useSocket() will create a new WS instance 
                     each time. Consider declaring it only once in the
                     upper level (root parent component), or find another
-                    possible solution like useContext.
+                    possible solution like useContext, or make useSocket
+                    a singleton.
     */
   const [input, setInput] = useState<string>("");
 
-  const handleClick = () => {
+  const handleSendMessage = () => {
     if (socket) {
       socket.emit("on_message", input);
     }
+    setInput("");
   };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSendMessage()
+    }
+  }
 
   return (
     <>
       <div className="flex bg-blue-100 p-10">
         <input
           value={input}
+          onKeyDown={handleKeyDown}
           onChange={(e) => setInput(e.target.value)}
           className="border-2 border-slate-500 rounded p-2"
         />
-        <button onClick={handleClick}>Send</button>
+        <button onClick={handleSendMessage}>Send</button>
       </div>
     </>
   );
 }
+
+export default ChatBox;

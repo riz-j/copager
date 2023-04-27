6534b768dc421aa162991f7ebc19c1cf76db4acb
riz-j <adiputra979@gmail.com>
2023-04-27 14:15:23 +1000

    Checkpoint

--- test-client/src/components/MessageBubble.component.tsx
+++ test-client/src/components/MessageBubble.component.tsx
@@ -7,8 +7,8 @@ interface MessageBubbleProps {
 const MessageBubble: React.FC<MessageBubbleProps> = (props) => {
     return (
         <>
-            <div>
-                <p>{props.message.message}</p>
+            <div className="bg-blue-100 my-1 mx-10 py-2 px-3 rounded-xl w-1/2">
+                {props.message.message}
             </div>
         </>
     )

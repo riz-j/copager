65e4567a0df0548cba677f8866677b7e727a94fa
riz-j <adiputra979@gmail.com>
2023-04-27 14:06:43 +1000

    Chat feed is now working: able to display messages on the screen

--- test-client/src/containers/ChatFeed.container.tsx
+++ test-client/src/containers/ChatFeed.container.tsx
@@ -1,16 +1,23 @@
 import MessageBubble from "components/MessageBubble.component";
 import { SocketContext } from "contexts/SocketContext";
 import { Message } from "models/Message";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const ChatFeed: React.FC = () => {
     const socket = useContext(SocketContext);
     const [messages, setMessages] = useState<Message[]>([]);
 
-    socket?.on('onMessage', (message: Message) => {
-        setMessages((prevMessages) => [...prevMessages, message])
-    })
+    useEffect(() => {
+        socket?.on('onMessage', (message: Message) => {
+            setMessages((prevMessages) => [...prevMessages, message])
+            console.log(`Got message:   ${message.message}`)
+        })
 
+        return () => {
+            socket?.off('onMessage');
+        }
+    }, [socket])
+    
     return (
         <>
             <div>

3512af2db5fb0c756b382601be6e87ba3256ed0b
riz-j <adiputra979@gmail.com>
2023-04-29 14:54:20 +1000

    Persist user ID in LAN room

--- websocket/behavior/base/room.py
+++ websocket/behavior/base/room.py
@@ -9,10 +9,9 @@ rooms = db["rooms"]
 async def on_join_lan_room(sid, user_id, room_id):  
     try:
         room = rooms.find_one({ "_id": room_id })
-        print(f"The ROOM is... {room}")
 
         if room is None:
-            # 1) Build a room
+
             new_room = {
                 "_id": room_id,
                 "type": "PUB_LAN",
@@ -20,25 +19,20 @@ async def on_join_lan_room(sid, user_id, room_id):
                 "users": []
             }
 
-            # 2) Insert that room into the database
-            created_room_id = rooms.insert_one(new_room)
-            print(created_room_id)
-            # 3) Insert user into that room
-            rooms.update_one({"_id": created_room_id.inserted_id}, {"$push": {"users": user_id}})
+            created_room = rooms.insert_one(new_room)
+            rooms.update_one({"_id": created_room.inserted_id}, {"$push": {"users": user_id}})
 
-            # 4) Make user join the room
             sio.enter_room(sid, room_id)
 
-            print(f"{sid} enter room {room_id}")
+            print(f"\n{sid} entered room {room_id}")
             return
         
         else:
-            # Check if user is already in the room
+
             if user_id not in room["users"]:
-                # Insert user into the room
+                
                 rooms.update_one({"_id": room["_id"]}, {"$push": {"users": user_id}})
 
-            # Make user join the room
             sio.enter_room(sid, room_id)
             
             print(f"\n{sid} enter room {room_id}")

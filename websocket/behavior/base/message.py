26150160e6d46ce4a5d84b081d208c3123fe33be
riz-j <adiputra979@gmail.com>
2023-04-29 16:47:30 +1000

    Save messages to the database

--- websocket/behavior/base/message.py
+++ websocket/behavior/base/message.py
@@ -1,5 +1,6 @@
 from server import sio 
 from data.database import db
+import pprint
 
 from model.Message import Message
 
@@ -8,28 +9,11 @@ messages = db["messages"]
 @sio.event
 async def on_message(sid, message):
     msg = Message(**message)
-    # msg = Message(
-    #     _id = "akwbak",
-    # )
     
     # Save the message to the database
-
-    # messages.insert_one()
+    saved_message = messages.insert_one(msg.dict(by_alias=True))
 
     # Check if the message is successfully saved in the database
-
-    # Emit the message to the room
-
-    print(msg.dict(by_alias=True))
-
-    # print(f"""
-    #     \n{sid}: 
-    #      id_:       {msg.id_}
-    #      Type:      {msg.type}
-    #      Message:   {msg.message}
-    #      Timestamp: {msg.timestamp} 
-    #      Sender:    {msg.sender}
-    #      Room:      {msg.room}
-    #     """)
-    
-    await sio.emit('onMessage', message, room=msg.room)
+    if saved_message.inserted_id == msg.id_:
+        pprint.pprint(message)
+        await sio.emit('onMessage', message, room=msg.room)

1d4c15975a5ce246aba972425e419c3e040bdfd3
riz-j <adiputra979@gmail.com>
2023-04-30 21:18:09 +1000

    Remove user from database if they disconnect

--- test-client/src/hooks/useSocket.tsx
+++ test-client/src/hooks/useSocket.tsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import { Socket, io } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
+import { usePubLanRoom } from "./usePubLanRoom";
 
 export const useSocket = () => {
     const URL: string = import.meta.env.VITE_WS_URI || 'http://0.0.0.0:8080';
     const [socket, setSocket] = useState<Socket | null>(null);
+    
+    /** Get the LAN room address of the client */
+    const { pubLanRoom } = usePubLanRoom();
 
     /** Get the Current User ID. */
     let currentUserId: string | null = localStorage.getItem("currentUserId");
@@ -16,21 +20,25 @@ export const useSocket = () => {
     }
 
     useEffect(() => {
-        const socketInstance = io(URL, {
-            query: {
-                USER_ID: currentUserId
+        if (pubLanRoom && currentUserId) {
+            const socketInstance = io(URL, {
+                query: {
+                    USER_ID: currentUserId,
+                    LAN_ROOM: pubLanRoom
+                }
+            });
+            setSocket(socketInstance);
+
+            socketInstance.on('connect', () => {
+                console.log(`Client connected to ${URL}`)
+            })
+        
+
+            return () => {
+                socketInstance.disconnect();
             }
-        });
-        setSocket(socketInstance);
-
-        socketInstance.on('connect', () => {
-            console.log(`Client connected to ${URL}`)
-        })
-
-        return () => {
-            socketInstance.disconnect();
         }
-    }, []);
+    }, [pubLanRoom, currentUserId]);
 
     return socket;
 }
--- websocket/behavior/base/basic.py
+++ websocket/behavior/base/basic.py
@@ -6,12 +6,14 @@ from dataclasses import asdict
 from model.Guest import Guest
 
 users = db["users"]
+rooms = db["rooms"]
 
 @sio.event
 async def connect(sid, environ):
     query_string = environ["QUERY_STRING"]
     query_params = parse_qs(query_string)
     user_id = query_params["USER_ID"][0]
+    lan_room = query_params["LAN_ROOM"][0]
 
     if user_id is None:
         return False
@@ -35,14 +37,28 @@ async def connect(sid, environ):
         except:
             raise Exception("Failed to be insert new user to database")
     
-    #Save the user_id as a value to the session object
-    await sio.save_session(sid, {"user_id": user_id})
+    # Save the user_id and lan_room as values to the session object
+    await sio.save_session(sid, {"user_id": user_id, "lan_room": lan_room})
+
     print(f"{sid} joined the server")
 
 
 @sio.event
-def disconnect(sid):
-    print(f"{sid} disconnected")
+async def disconnect(sid):
+    session = await sio.get_session(sid)
+    user_id = session["user_id"]
+    lan_room = session["lan_room"]
+
+    # Remove user ID reference from the LAN room document in the database
+    ack = rooms.update_one({"_id": lan_room}, {"$pull": {"users": user_id}})
+    
+    if ack.acknowledged == False:
+        raise Exception("Failed to remove User ID reference out of the LAN room document")
+
+    print(user_id + " left the room " + lan_room)
+    print(f"{user_id} disconnected")
+    # IMPROVEMENT: Only remove user_id from database if all of their sessions are closed
+    #              (user can have multiple browsers open at once).
 
 
 @sio.event
@@ -54,3 +70,4 @@ async def on_ping(sid):
 @sio.event
 async def on_echo(sid, message):
     sio.emit("onMessage", message, room=sid)
+

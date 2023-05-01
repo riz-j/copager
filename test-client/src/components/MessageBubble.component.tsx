2273ea30800d49529f3cc84827ca51a1e82048bb
riz-j <adiputra979@gmail.com>
2023-05-01 14:01:40 +1000

    Add custom alignment and bg-color accordingly

--- test-client/src/components/MessageBubble.component.tsx
+++ test-client/src/components/MessageBubble.component.tsx
@@ -1,13 +1,21 @@
+import { DataContext } from "contexts/DataContext";
 import { Message } from "models/Message";
+import { useContext } from "react";
 
 interface MessageBubbleProps {
     message: Message
 }
 
 const MessageBubble: React.FC<MessageBubbleProps> = (props) => {
+    const currentUserId: string = useContext(DataContext).currentUser._id;
+    const isCurrentUser: boolean = currentUserId === props.message.sender;
+    
+    const alignment: string = isCurrentUser ? "justify-end" : "justify-start";
+    const bgColor: string = isCurrentUser ? "bg-blue-200" : "bg-gray-200";
+
     return (
-        <>
-            <div className="bg-blue-100 my-1 mx-10 py-2 px-3 rounded-xl w-1/2">
+        <div className={`flex w-full ${alignment}`}>
+            <div className={`my-1 mx-10 py-2 px-3 rounded-xl w-1/2 ${bgColor}`}>
                 <p>{props.message.message}</p>
                 {/* <p>ID:   {props.message._id}</p> */}
                 {/* <p>Type:   {props.message.type}</p> */}
@@ -16,7 +24,7 @@ const MessageBubble: React.FC<MessageBubbleProps> = (props) => {
                 {/* <p>Sender:   {props.message.sender}</p>  */}
                 {/* <p>Room:   {props.message.room}</p>  */}
             </div>
-        </>
+        </div>
     )
 }
 

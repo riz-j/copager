dc94362600cc1d16095e3c7ce5c01018dd8987fe
riz-j <adiputra979@gmail.com>
2023-05-01 12:33:09 +1000

    Refactor to lan_room

--- websocket/behavior/lan/lan_room.py
+++ websocket/behavior/lan/lan_room.py
@@ -7,32 +7,33 @@ rooms = db["rooms"]
 messages = db["messages"]
 
 @sio.event
-async def on_join_lan_room(sid, room_id):  
+async def on_join_lan_room(sid):  
     session = await sio.get_session(sid)
     user_id = session["user_id"]
+    lan_room = session["lan_room"]
 
     try:
-        room = rooms.find_one({ "_id": room_id })
+        room = rooms.find_one({ "_id": lan_room })
 
         if room is None:
             new_room = Room(
-                _id=room_id,
+                _id=lan_room,
                 type="PUBLIC_LAN",
             )
             created_room = rooms.insert_one(asdict(new_room))
             rooms.update_one({"_id": created_room.inserted_id}, {"$push": {"users": user_id}})
 
-            sio.enter_room(sid, room_id)
+            sio.enter_room(sid, lan_room)
 
-            print(f"\n{user_id} entered room {room_id}")
+            print(f"\n{user_id} entered room {lan_room}")
 
         else:
             if user_id not in room["users"]:
                 rooms.update_one({"_id": room["_id"]}, {"$push": {"users": user_id}})
 
-            sio.enter_room(sid, room_id)
+            sio.enter_room(sid, lan_room)
             
-            print(f"\n{user_id} entered room {room_id}")
+            print(f"\n{user_id} entered room {lan_room}")
 
 
     except Exception as e:

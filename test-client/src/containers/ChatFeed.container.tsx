2f8772af8a49e8d07f8836fdf01f0d1e39cdf60a
riz-j <adiputra979@gmail.com>
2023-05-01 14:33:33 +1000

    Scroll to bottom of screen everytime there is a new chat

--- test-client/src/containers/ChatFeed.container.tsx
+++ test-client/src/containers/ChatFeed.container.tsx
@@ -1,16 +1,25 @@
 import MessageBubble from "components/MessageBubble.component";
 import { DataContext } from "contexts/DataContext";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 
 const ChatFeed: React.FC = () => {
     const messages = useContext(DataContext).messages;
     
+    const bottom = useRef<HTMLDivElement | null>(null)
+    useEffect(() => {
+        if (bottom.current) {
+
+            bottom.current.scrollIntoView({ behavior: "smooth" })
+        }
+    }, [messages])
+
     return (
         <>
             <div className="bg-gray-100">
                 { messages.map(msg => {
                     return <MessageBubble key={msg._id} message={msg} />
                 }) }
+                <div ref={bottom}></div>
             </div>
         </>
     )

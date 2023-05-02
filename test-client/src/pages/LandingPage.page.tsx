0496e5acdc07e0726ff9947403b6d19c19697e18
riz-j <adiputra979@gmail.com>
2023-05-02 20:12:16 +1000

    Add github icon

--- test-client/src/pages/LandingPage.page.tsx
+++ test-client/src/pages/LandingPage.page.tsx
@@ -1,5 +1,6 @@
 import './LandingPage.css'
 import chat_icon from 'assets/chat-lines.png'
+import github_icon from 'assets/github-icon.png'
 
 const LandingPage: React.FC = () => {
     const debug: boolean = false;
@@ -19,14 +20,17 @@ const LandingPage: React.FC = () => {
                     <p className='text-black/60 bg-lime-200 p-5'>
                         Copager is an online LAN chat app that allows you to chat with people that are connected to the same wifi network as you.
                     </p>
-                    <div className="flex flex-col w-full gap-3 bg-lime-200 px-5">
+                    <div className="flex flex-col w-full gap-4 bg-lime-200 px-5">
                         <div className="flex justify-center items-center gap-2 bg-purple-500 w-full h-12 border-2 border-black shadow-solid-primary">
                             <div className='pb-[0.1rem]'>
-                                <img src={chat_icon} className='h-[1.1rem]'/>
+                                <img src={chat_icon} className='h-[1.2rem]'/>
                             </div>
                             <p className='font-bold text-white'>OPEN CHAT</p>
                         </div>
-                        <div className="flex justify-center items-center bg-lime-200 border-2 border-black w-full h-12 shadow-solid-primary">
+                        <div className="flex justify-center items-center gap-[0.4rem] bg-lime-200 border-2 border-black w-full h-12 shadow-solid-primary">
+                            <div className='pb-[0.1rem]'>
+                                <img src={github_icon} className='h-5' />
+                            </div>
                             <p className='font-bold text-black'>VIEW ON GITHUB</p>
                         </div>
                     </div>

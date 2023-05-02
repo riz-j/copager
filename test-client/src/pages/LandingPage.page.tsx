0b03dea991587662486dafd54ec70c6f3dc45860
riz-j <adiputra979@gmail.com>
2023-05-02 17:47:35 +1000

    Improve layout

--- test-client/src/pages/LandingPage.page.tsx
+++ test-client/src/pages/LandingPage.page.tsx
@@ -5,22 +5,21 @@ const LandingPage: React.FC = () => {
 
     return (
         <>
-            <div className="flex flex-col h-screen p-5">
+            <div className="flex flex-col h-screen p-5 bg-white">
                 <div className={`flex items-center h-28 ${debug && "bg-orange-100"}`}>
-                    <h1 className="text-3xl font-bold">
-                        <span className="text-red-500">co</span>
-                        <span className="text-red-500">pager</span>
+                    <h1 className="text-3xl font-bold mx-3 text-red-500">
+                        copager
                     </h1>
                 </div>
-                <div className={`flex flex-col justify-end items-center gap-5 ${debug && "bg-pink-100"} h-96 text-center`}>
-                    <h1 className="text-4xl font-bold">Talk with People Connected to the same Wifi as you</h1>
-                    <p>Copager is an online LAN chat app that allows you to chat with people that are connected to the same wifi network as you.</p>
+                <div className={`flex flex-col justify-end items-center gap-5 ${debug && "bg-rose-100"} h-96 text-center`}>
+                    <h1 className="text-4xl font-bold">Talk with People <span className='text-red-500'>Connected</span> to the same <span className='text-red-500'>Wifi</span> as you</h1>
+                    <p className='text-gray-500'>Copager is an online LAN chat app that allows you to chat with people that are connected to the same wifi network as you.</p>
                     <div className="flex flex-col w-full gap-3">
-                        <div className="flex justify-center items-center border border-gray-500 w-full h-10">
-                            <p>Open Chat</p>
+                        <div className="flex justify-center items-center border bg-red-500 w-full h-10">
+                            <p className='font-bold text-white'>OPEN CHAT</p>
                         </div>
-                        <div className="flex justify-center items-center border border-gray-500 w-full h-10">
-                            <p>View on GitHub</p>
+                        <div className="flex justify-center items-center border border-red-500 w-full h-10">
+                            <p className='font-bold text-red-500'>VIEW ON GITHUB</p>
                         </div>
                     </div>
                 </div>

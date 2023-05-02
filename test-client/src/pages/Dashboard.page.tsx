b921d10ab1e7d19f627461bd675fa7a52b1ab984
riz-j <adiputra979@gmail.com>
2023-05-02 16:09:48 +1000

    Only show left sidebar on lg screens

--- test-client/src/pages/Dashboard.page.tsx
+++ test-client/src/pages/Dashboard.page.tsx
@@ -7,17 +7,17 @@ const Dashboard: React.FC = () => {
         <>
             <div className="flex">
                 <div className="fixed h-screen bg-red-500 hidden 
-                                md:block md:w-80 lg:w-96">  {/** For Desktop and Tablets */}
+                                lg:block lg:w-96">  {/** For Desktop and Horizontal Tablets */}
                     <p>Hello</p>
                 </div>
                 <div className="flex flex-col w-full 
-                                md:ml-80 lg:ml-96"> 
+                                lg:ml-96"> 
                     <div className="relative flex-1">
                         <ChatFeed/> 
                     </div>
                     
                     <div className="fixed bottom-0 right-0 w-full
-                                    md:left-80 lg:left-96 md:w-auto"> 
+                                    lg:left-96 lg:w-auto"> 
                         <ChatBox/> 
                     </div>
                 </div>
@@ -27,3 +27,27 @@ const Dashboard: React.FC = () => {
 }
 
 export default Dashboard;
+
+
+
+
+        // ----------------------------------------------------------------------------
+        //  Use the template below to have custom reponsiveness for md and lg screens
+        // ----------------------------------------------------------------------------
+        // <div className="flex">
+        //     <div className="fixed h-screen bg-red-500 hidden 
+        //                     md:block md:w-80 lg:w-96">  {/** For Desktop and Tablets */}
+        //         <p>Hello</p>
+        //     </div>
+        //     <div className="flex flex-col w-full 
+        //                     md:ml-80 lg:ml-96"> {/** For Desktop and Tablets */}
+        //         <div className="relative flex-1">
+        //             <ChatFeed/> 
+        //         </div>
+                
+        //         <div className="fixed bottom-0 right-0 w-full
+        //                         md:left-80 lg:left-96 md:w-auto"> {/** For Desktop and Tablets */}
+        //             <ChatBox/> 
+        //         </div>
+        //     </div>
+        // </div>

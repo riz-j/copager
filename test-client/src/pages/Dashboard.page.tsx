41a11b28f50a44881181068c619b041e9513cd93
riz-j <adiputra979@gmail.com>
2023-05-02 16:03:56 +1000

    Improve responsiveness

--- test-client/src/pages/Dashboard.page.tsx
+++ test-client/src/pages/Dashboard.page.tsx
@@ -6,18 +6,18 @@ const Dashboard: React.FC = () => {
     return (
         <>
             <div className="flex">
-                <div className="fixed h-screen bg-red-500 w-96 hidden 
-                                md:block">  {/** For Desktop and Tablets */}
+                <div className="fixed h-screen bg-red-500 hidden 
+                                md:block md:w-80 lg:w-96">  {/** For Desktop and Tablets */}
                     <p>Hello</p>
                 </div>
                 <div className="flex flex-col w-full 
-                                md:ml-96"> 
+                                md:ml-80 lg:ml-96"> 
                     <div className="relative flex-1">
                         <ChatFeed/> 
                     </div>
                     
                     <div className="fixed bottom-0 right-0 w-full
-                                    md:left-96 md:w-auto"> 
+                                    md:left-80 lg:left-96 md:w-auto"> 
                         <ChatBox/> 
                     </div>
                 </div>

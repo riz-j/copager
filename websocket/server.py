9459c5a4a9c326718f6661d7faaf8b458f2d6150
riz-j <adiputra979@gmail.com>
2023-05-03 19:25:58 +1000

    Add additional cors

--- websocket/server.py
+++ websocket/server.py
@@ -1,3 +1,3 @@
 import socketio
 
-sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins='http://localhost:5070')
+sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins=['http://localhost:5070', 'http://0.0.0.0:5070', 'http://copager-react:5070', 'http://copager-react', 'http://170.64.176.135', 'http://170.64.176.135:5070', 'http://copager.com', 'https://copager.com'])

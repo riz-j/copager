b2d4fa6ab94044b5feccdfcb5676585bce5b59f1
riz-j <adiputra979@gmail.com>
2023-05-04 13:40:10 +1000

    Configure for Socketio Swift Client - Make cors accept all origins

--- websocket/behavior/base/echo.py
+++ websocket/behavior/base/echo.py
@@ -3,10 +3,10 @@ from server import sio
 @sio.event
 async def on_ping(sid):
     print(f"PING from {sid}")
-    sio.emit("onMessage", "PING from server")
+    await sio.emit("onMessage", "PING from server")
 
 
 @sio.event
 async def on_echo(sid, message):
-    sio.emit("onMessage", message, room=sid)
+    await sio.emit("onMessage", message, room=sid)
 

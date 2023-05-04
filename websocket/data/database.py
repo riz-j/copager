af251532aee83da0748a4fed26e938462c14ddc0
riz-j <adiputra979@gmail.com>
2023-05-04 20:37:14 +1000

    Fix Issue: Make message documents expire after 24 hours

--- websocket/data/database.py
+++ websocket/data/database.py
@@ -14,6 +14,14 @@ db = client["copager"]
 # Instatiate collection object
 messages = db["messages"]
 
-# Create Index and define TTL 
-messages.create_index("timestamp", expireAfterSeconds=86400)
+# Check if index already exist
+index_exist = False
+messages_indexes = messages.list_indexes()
+for index in messages_indexes:
+    if index["name"] == "timestamp_exp":
+        index_exist = True
+
+if index_exist == False:
+    # Create Index and define TTL 
+    messages.create_index("timestamp", expireAfterSeconds=86400, name="timestamp_exp")
 

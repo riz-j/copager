9d9c95fb28d75ed550e623f2b9e635c97badfd5c
riz-j <adiputra979@gmail.com>
2023-06-05 14:27:50 +1000

    Make file uploader use bytes so that SwiftUI can read it

--- file-uploader/main.py
+++ file-uploader/main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, File, UploadFile, APIRouter, HTTPException
+from fastapi import FastAPI, File, UploadFile, APIRouter, HTTPException, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from environ import cors_origins, forbidden_file_extensions, root_url
 import time
 import os
+import re
 from uuid import uuid4
 
 app = FastAPI()
 
-app.mount("/files", StaticFiles(directory="files"), name="files")
+# app.mount("/files", StaticFiles(directory="files"), name="files")
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,6 +18,42 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+@app.get("/files/{file_name}")
+async def get_file(request: Request, file_name: str):
+    path = f"files/{file_name}"
+    if not os.path.exists(path):
+        return Response(status_code=404)
+
+    file_size = os.path.getsize(path)
+    range_header = request.headers.get('Range', None)
+
+    if range_header:
+        byte1, byte2 = 0, None
+        m = re.search('(\d+)-(\d*)', range_header)
+        g = m.groups()
+
+        byte1 = int(g[0]) if g[0] else 0
+        byte2 = int(g[1]) if g[1] else file_size - 1
+
+        if byte1 >= file_size:
+            return Response(status_code=416)
+
+        with open(path, 'rb') as f:
+            f.seek(byte1)
+            body = f.read(byte2 - byte1 + 1)
+
+        resp = Response(body, status_code=206, media_type='video/mp4')
+        resp.headers["Content-Range"] = f"bytes {byte1}-{byte2}/{file_size}"
+    else:
+        with open(path, 'rb') as f:
+            body = f.read()
+
+        resp = Response(body, media_type='video/mp4')
+        resp.headers["Content-Length"] = str(file_size)
+
+    resp.headers["Accept-Ranges"] = "bytes"
+    return resp
 
 
 @app.get("/hello")
